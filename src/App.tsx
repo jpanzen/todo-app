0943fe85d2b2cd110f03eaf84c4510681ca0e32c
Jan Panzenberger <jan@panzenberger.cz>
2024-06-29 21:04:51 +0200

    finito - poznamky a implementace paměti

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
 import Todo from './Todo'
-import { AppShell, Burger } from '@mantine/core'
+import { AppShell } from '@mantine/core'
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
 function App() {
-  const [opened, { toggle }] = useDisclosure();
-  const [count, setCount] = useState(0)
-
   return (
     <MantineProvider defaultColorScheme="dark">
       <AppShell padding="lg">
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,41 +1,59 @@
 // @ts-check
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Flex, Button, Group, Center, ActionIcon, Input, CloseButton } from '@mantine/core';
 import { IconCheck, IconArrowDown, IconArrowUp } from '@tabler/icons-react';
 import { Task } from './models/Task';
 
 export default function Todo() {
-  const [text, setText] = useState<string>('');
-  const [tasks, setTasks] = useState<Task[]>([
-    { id: 1, title: "Vyvencit psa" },
-    { id: 2, title: "Vyvencit kocku" },
-    { id: 3, title: "Uvarit obed pro psa" },
-    { id: 4, title: "Uvarit obed pro kocku" }
-  ]);
+  const [text, setText] = useState('');
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const tasksLocal = localStorage.getItem('tasks');
+    // Pokud existuje něco v localStorage, načtou se data z localStorage, pokud ne -> defaultní hodnoty
+    return tasksLocal ? JSON.parse(tasksLocal) : [
+      {id:1, title: "Uvařit guláš"},
+      {id:2, title: "Vyvenčit psa"},
+      {id:3, title: "Dát si pivko"}
+    ];
+  });
 
+  // Pokud nastane změna v tasks v appce, tak dojde k přepsání tasks v localStorage paměti
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  // Funkce obsluhující tlačítko pro posouvání úkolů nahoru
   function moveTaskUp(index: number) {
     if (index === 0) return; // První úkol nelze přesunout nahoru
-    const newTasks = [...tasks];
-    [newTasks[index - 1], newTasks[index]] = [newTasks[index], newTasks[index - 1]];
+    const newTasks = [...tasks]; // Vytvoření kopie pole s úkoly
+    [newTasks[index - 1], newTasks[index]] = [newTasks[index], newTasks[index - 1]]; // Switch úkolů
     setTasks(newTasks);
   }
   
+  // Funkce obsluhující tlačítko pro posouvání úkolů dolů
   function moveTaskDown(index: number) {
     if (index === tasks.length - 1) return; // Poslední úkol nelze přesunout dolů
-    const newTasks = [...tasks];
-    [newTasks[index + 1], newTasks[index]] = [newTasks[index], newTasks[index + 1]];
+    const newTasks = [...tasks]; // Vytvoření kopie pole s úkoly
+    [newTasks[index + 1], newTasks[index]] = [newTasks[index], newTasks[index + 1]]; // Switch úkolů
     setTasks(newTasks);
   }
 
+  // Funkce obsluhující tlačítko pro dokončení úkolů
+  function finishTask(id: number){
+    const updatedTasks = tasks.filter(task => task.id !== id); // Kopie pole s filtrovacím pravidlem, kde se kopírují pouze ty které nemají mazané id úkolu
+    setTasks(updatedTasks);
+  }
+
+  // Funkce obsluhující přidávání úkolů
   function addTask(title: string) {
-    if (title === '') return;
+    if (title === '') return; // Nemůžeme přidat prázdný úkol
+    // Zde vytvářím nový objekt
     const newTask = {
       id: Date.now(),
       title
     };
-    setTasks([...tasks, newTask]);
-    setText('');
+    setTasks([...tasks, newTask]); // Nový task se appenduje ke stávajícím
+    setText(''); // "Vynulování" pole s textem
   }
 
   return (
@@ -73,7 +91,7 @@ export default function Todo() {
               <ActionIcon onClick={() => moveTaskDown(index)} color="blue" variant="light">
                 <IconArrowDown size={20} />
               </ActionIcon>
-              <ActionIcon color="green" variant="light">
+              <ActionIcon onClick={() => finishTask(task.id)} color="green" variant="light">
                 <IconCheck size={20} />
               </ActionIcon>
             </Flex>
